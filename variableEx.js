f7f0dbc1dc81fa86e55e4366303b22adda3f49c2
Vishal Zade <vishalzade75@gmail.com>
2025-11-26 15:49:22 +0530

    Adding boolean value

--- variableEx.js
+++ variableEx.js
@@ -1,3 +1,8 @@
 var fname = "Vishal";
 fname = "Hellow world";
 console.log(fname);
+var isActive = true;
+var scores = [23, 45, 67, 89];
+console.log(fname);
+console.log(isActive);
+console.log(scores);
--- variableEx.ts
+++ variableEx.ts
@@ -1,3 +1,10 @@
 let fname:string = "Vishal";
 fname = "Hellow world";
 console.log(fname);
+let isActive:boolean = true;
+let scores: number[] = [23,45,67,89];
+
+console.log(fname);
+console.log(isActive);
+console.log(scores);
+
